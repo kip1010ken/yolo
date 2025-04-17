e3c6a08849bc663c292deb45980c6106369e2b80
Kenneth Kiptenten <tentenkenneth@outlook.com>
2025-04-18 00:15:53 +0300

    update server.js to connect to the mongo db running in the db container

--- backend/server.js
+++ backend/server.js
@@ -7,8 +7,8 @@ const upload = multer();
 const productRoute = require('./routes/api/productRoute');
 
 // Connecting to the Database
-let mongodb_url = 'mongodb://localhost/';
-let dbName = 'yolomy';
+let mongodb_url = 'mongodb://mongodb:27017/';
+let dbName = 'yolodb';
 
 // define a url to connect to the database
 const MONGODB_URI = process.env.MONGODB_URI || mongodb_url + dbName
